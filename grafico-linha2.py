f72dabc1dfc57769f9dbcb7a55b58b7828631440
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-07-25 11:45:48 -0300

    Create grafico-linha2.py

---
+++ grafico-linha2.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt 
+
+x = [1,2,5]
+y = [2,3,7]
+
+plt.title("Meu primeiro grafico com Python")
+
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+
+
+plt.plot(x, y)
+plt.show()
